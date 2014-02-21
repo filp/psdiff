6062d678ece90ad8ff3dba579d40b1f83a5945ba
Gabriele Cirulli <gabrycir@gmail.com>
2014-02-21 15:03:01 +0100

    Add simple install script

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e # Fail if any command exits with nonzero status
+set -v # Show all commands when running them
+
+if [[ ! -d .git ]] then
+  echo "Hey! You need to run this in the root of a git repository!"
+  exit
+fi
+
+mkdir -p ".git/hooks" # We need this if it doesn't exist!
+cd ".git/hooks"
+
+# Get ze file
+curl "https://raw2.github.com/filp/psdiff/master/git/hooks/pre-commit" -o "pre-commit"
+
+# Add execution bits to the user
+
